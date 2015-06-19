ca8ac7f27f6b7390089157070a5880f67f9f22c0
Srinivas Kommu <srini@github.com>
2015-06-19 01:07:04 -0700

    test

---
+++ milan-app/src/main/java/milan/app/MilanproductApplication.java
@@ -0,0 +1,12 @@
+package milan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilanproductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilanproductApplication.class, args);
+    }
+}
