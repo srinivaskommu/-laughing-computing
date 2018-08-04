cb179ebf55f129e291bffe121fdb1a0fe977ed03
arjun kommu <srini903@gmail.com>
2018-08-04 06:59:58 -0700

    vcraftech-demo/

---
+++ vcraftech-demo/src/main/java/com/vcraftech/demo/vcraftechdemo/VcraftechDemoApplication.java
@@ -0,0 +1,12 @@
+package com.vcraftech.demo.vcraftechdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcraftechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcraftechDemoApplication.class, args);
+	}
+}
